88a733c811398d652a0638c418eb25df5a01d128
valeria campilongo <cvaleriac@hotmail.it>
2019-05-15 16:47:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign({}, driver, { [key]: value })
+    return Object.assign({}, driver, { [key]: value });
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  driver[key]=value;
-  return driver;
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver;
 }
 
-function deleteFromDriverByKey(driver, key){
- var copy = Object.assign({},driver);
- delete copy[key];
- return copy;
+function deleteFromDriverByKey(driver, key) {
+    const newDriver = Object.assign({}, driver);
+    delete newDriver[key];
+    return newDriver;
 }
 
-function destructivelyDeleteFromDriverByKey(driver,key){
-  delete driver[key];
-  return driver;
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
 }
